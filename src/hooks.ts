247064f525090382ff5272d2b6c25f33f605170c
Francesco Giovannini <fgiova@fgiova.com>
2023-11-20 09:00:39 +0100

    Add some hooks

---
+++ src/hooks.ts
@@ -0,0 +1,45 @@
+export type HookName = "onPoll" |
+	"onMessage" |
+	"onHandlerSuccess" |
+	"onHandlerTimeout" |
+	"onHandlerError" |
+	"onSuccess" |
+	"onSQSError";
+
+export class Hooks {
+	private hookSymbols = {
+		onPoll: Symbol("poll"),
+		onMessage: Symbol("message"),
+		onHandlerSuccess: Symbol("handlerSuccess"),
+		onHandlerTimeout: Symbol("handlerTimeout"),
+		onHandlerError: Symbol("handlerError"),
+		onSuccess: Symbol("success"),
+		onSQSError: Symbol("sqsError"),
+	}
+	private hooks: Record<symbol, Function[]>;
+
+	constructor() {
+		this.hooks = {
+			[this.hookSymbols.onPoll]: [],
+			[this.hookSymbols.onMessage]: [],
+			[this.hookSymbols.onHandlerSuccess]: [],
+			[this.hookSymbols.onHandlerTimeout]: [],
+			[this.hookSymbols.onHandlerError]: [],
+			[this.hookSymbols.onSuccess]: [],
+			[this.hookSymbols.onSQSError]: [],
+		};
+	}
+	public addHook(hook: HookName, fn: Function) {
+		const hookSymbol = this.hookSymbols[hook];
+		if(!hookSymbol) throw new Error(`Invalid hook ${hook}`);
+		this.hooks[hookSymbol]?.push(fn);
+	}
+
+	public async runHook(hook: HookName, ...args: any[]) {
+		const hookSymbol = this.hookSymbols[hook];
+		if(!hookSymbol) throw new Error(`Invalid hook ${hook}`);
+		for (const fn of this.hooks[hookSymbol]!) {
+			await fn(...args);
+		}
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
 import {Signer, SignerOptions} from "@fgiova/aws-signature";
 import type {Pool} from "undici";
 import {MiniSQSClient, Message} from "@fgiova/mini-sqs-client";
+import {HookName, Hooks} from "./hooks";
 
 export class SQSConsumer {
+
+	private readonly queueARN: string;
+	private readonly handlerOptions: {
+		deleteMessage?: boolean,
+		extendVisibilityTimeout?: boolean,
+		executionTimeout?: number,
+	};
+	private readonly clientOptions: {
+		destroySigner?: boolean,
+	};
+	private readonly consumerOptions: {
+		deleteMessage?: boolean,
+		visibilityTimeout?: number,
+		waitTimeSeconds?: number,
+		itemsPerRequest?: number,
+		messageAttributeNames?: string[],
+	};
+	private readonly sqsClient: MiniSQSClient;
+	private readonly hooks: Hooks;
 
 	constructor(options:{
 		queueARN: string,
@@ -27,13 +47,32 @@ export class SQSConsumer {
 			messageAttributeNames?: string[],
 		},
 		hooks?: {
+			onPoll?: (messages: Message[]) => Promise<Message[]>,
 			onMessage?: (message: Message) => Promise<Message>,
 			onHandlerSuccess?: (message: Message) => Promise<Message>,
 			onHandlerTimeout?: (message: Message) => Promise<Boolean>,
 			onHandlerError?: (message: Message, error: Error) => Promise<Boolean>,
 			onSuccess?: (message: Message) => Promise<Boolean>,
-			onError?: (message: Message, error: Error) => Promise<Boolean>,
-		}}) {
+			onError?: ( hook: HookName, message: Message, error: Error) => Promise<Boolean>,
+			onSQSError?: (error: Error, message?: Message) => Promise<void>,
+	}}) {
+
+
+		if(!options.queueARN) throw new Error("queueARN is required");
+		if(!options.handler || typeof options.handler !== "function") throw new Error("handler is required and must be a function");
+
+		this.hooks = new Hooks();
+		this.queueARN = options.queueARN;
+		this.clientOptions = { destroySigner: options.clientOptions?.destroySigner ?? true };
+
+
+		if (options.hooks){
+			for (const [key, value] of Object.entries(options.hooks)) {
+				if(typeof value !== "function") throw new Error(`${key} must be a function`);
+				this.hooks.addHook(key as HookName, value);
+			}
+		}
+		this.hooks.addHook("onMessage", options.handler);
 
 	}
 
